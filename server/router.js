81fa0a357a48193da0ef8c90010ef3727decd5d3
Dario Vladovic <d.vladimyr@gmail.com>
2018-03-30 15:13:09 +0200

    Public ping route example

--- server/router.js
+++ server/router.js
@@ -6,6 +6,8 @@ const course = require('./course');
 const user = require('./user');
 
 const router = express.Router();
+// TODO: Remove this demo route!
+router.use('/ping', (_, res) => res.jsend.success(null));
 router.use(course.path, auth, course.router);
 router.use(user.path, user.router);
 
