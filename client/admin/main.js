c105296758f851bcca3952aa4438ee1deae40155
Damir Perisic <underscope@gmail.com>
2018-04-03 06:46:44 +0200

    Admin SPA init updates
    - Validation config
    - Added Vuetify
    - Added admin store

--- client/admin/main.js
+++ client/admin/main.js
@@ -1,13 +1,23 @@
-import Vue from 'vue';
-
-// import store from './store';
-// import router from './router';
 import App from './App';
+import router from './router';
+import store from './store';
+import VeeValidate from '@/common/validation';
+import Vuetify from 'vuetify';
+import Vue from 'vue';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify);
+
+Vue.use(VeeValidate, {
+  delay: 700,
+  fieldsBagName: 'vFields',
+  errorBagName: 'vErrors'
+});
 
 // eslint-disable-next-line no-new
 new Vue({
-  // router,
-  // store,
+  store,
+  router,
   el: '#app',
   render: h => h(App)
 });
