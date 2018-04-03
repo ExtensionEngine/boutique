6140116b75070dea0c99978d67c46c998ed1ebd7
Damir Perisic <underscope@gmail.com>
2018-04-03 06:30:49 +0200

    Created basic auth router

---
+++ client/admin/router.js
@@ -0,0 +1,38 @@
+import get from 'lodash/get';
+import Router from 'vue-router';
+import store from './store';
+import Vue from 'vue';
+
+import Auth from '@/common/components/auth/index';
+import Home from '@/admin/components/Home';
+import Login from '@/common/components/auth/Login';
+
+Vue.use(Router);
+
+const router = new Router({
+  routes: [{
+    path: '/',
+    name: 'home',
+    component: Home,
+    meta: { auth: true }
+  }, {
+    path: '/',
+    name: 'auth',
+    component: Auth,
+    children: [{
+      path: 'login',
+      name: 'login',
+      component: Login
+    }]
+  }]
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(it => it.meta.auth) && !get(store.state, 'auth.user')) {
+    next({ name: 'login' });
+  } else {
+    next();
+  }
+});
+
+export default router;
