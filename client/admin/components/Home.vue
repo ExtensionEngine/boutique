b5854b3ab4de2a06470c8e03d39c3e48412f103e
Damir Perisic <underscope@gmail.com>
2018-04-03 07:11:21 +0200

    Created default admin page

---
+++ client/admin/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p class="message">
+      <span class="mdi mdi-message"></span>
+      Hello admin!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+};
+</script>
+
+<style lang="scss" scoped>
+.message {
+  text-align: center;
+}
+</style>
