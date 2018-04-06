07dca95501040dd085864c1ab1a553614db8ae0f
Damir Perisic <underscope@gmail.com>
2018-04-06 17:20:46 +0200

    Minor navbar cleanup

--- client/admin/components/common/Navbar.vue
+++ client/admin/components/common/Navbar.vue
@@ -1,29 +1,26 @@
 <template>
-  <nav class="navbar is-fixed-top is-light" role="navigation" aria-label="main navigation">
+  <nav
+    class="navbar is-fixed-top is-light"
+    role="navigation"
+    aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
-        <img src="https://bulma.io/images/bulma-logo.png" alt="LMS Starter App" width="112" height="28">
-      </a>
+      <a class="navbar-item" href="/admin/#/">LMS ADMIN</a>
     </div>
     <div class="navbar-menu">
       <div v-if="user" class="navbar-end">
-        <div class="navbar-item">{{user.email}}</div>
-        <a href="#" @click="logout" class="navbar-item">Logout</a>
+        <div class="navbar-item">{{ user.email }}</div>
+        <a @click="logout" href="#" class="navbar-item">Logout</a>
       </div>
     </div>
   </nav>
 </template>
 
 <script>
-  import { mapState, mapActions } from 'vuex';
+import { mapState, mapActions } from 'vuex';
 
-  export default {
-    name: 'Navbar',
-    computed: {
-      ...mapState('auth', ['user'])
-    },
-    methods: {
-      ...mapActions('auth', ['logout'])
-    }
-  };
+export default {
+  name: 'Navbar',
+  computed: mapState('auth', ['user']),
+  methods: mapActions('auth', ['logout'])
+};
 </script>
