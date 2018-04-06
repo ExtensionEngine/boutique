7f3daeb7f631a9dfc1c0edb34958b8a786e47778
Damir Perisic <underscope@gmail.com>
2018-04-06 17:25:21 +0200

    Sidebar cleanup :lipstick:

--- client/admin/components/common/Sidebar.vue
+++ client/admin/components/common/Sidebar.vue
@@ -1,54 +1,13 @@
 <template>
   <aside class="menu">
-    <p class="menu-label">
-      Actions
-    </p>
+    <p class="menu-label">Actions</p>
     <ul class="menu-list">
-      <li>
-        <router-link to="/"> Dashboard </router-link>
-      </li>
-      <li>
-        <router-link to="/users"> Users </router-link>
-      </li>
+      <li><router-link to="/">Dashboard</router-link></li>
     </ul>
-
-    <p class="menu-label">
-      Administration
-    </p>
+    <p class="menu-label">Administration</p>
     <ul class="menu-list">
-      <li>
-        Programs
-
-        <ul>
-          <li>
-            <router-link to="/programs/new"> Add new </router-link>
-          </li>
-          <li>
-            <router-link to="/programs"> Show All </router-link>
-          </li>
-        </ul>
-      </li>
-      <li>
-        <router-link to="/districts"> Districts </router-link>
-      </li>
-      <li>
-        <router-link to="/schools"> Schools </router-link>
-      </li>
-    </ul>
-
-    <p class="menu-label">
-      Reports
-    </p>
-    <ul class="menu-list">
-      <li>
-        <a>Districts</a>
-      </li>
-      <li>
-        <a>Learners</a>
-      </li>
-      <li>
-        <a>Tools</a>
-      </li>
+      <li><router-link to="/users">Users</router-link></li>
+      <li><router-link to="/programs">Programs</router-link></li>
     </ul>
   </aside>
 </template>
