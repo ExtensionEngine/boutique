b8073c5e9e301cf7c09004b348d0e5223ab77ba2
Damir Perisic <underscope@gmail.com>
2018-04-06 17:22:35 +0200

    Cleanup :lipstick:

--- client/admin/components/common/NotFound.vue
+++ client/admin/components/common/NotFound.vue
@@ -1,21 +1,21 @@
 <template>
   <section class="section is-medium">
-    <div class="title"> 404 </div>
-    <div class="subtitle"> Page not found </div>
-    <router-link to="/"> Go Home </router-link>
+    <div class="title">404</div>
+    <div class="subtitle">Page not found</div>
+    <router-link to="/">Go Home</router-link>
   </section>
 </template>
 
 <style lang="scss" scoped>
-  .title {
-    font-size: 1000%;
-  }
+.title {
+  font-size: 1000%;
+}
 
-  .subtitle {
-    font-size: 300%;
-  }
+.subtitle {
+  font-size: 300%;
+}
 
-  a {
-    font-size: 150%;
-  }
+a {
+  font-size: 150%;
+}
 </style>
