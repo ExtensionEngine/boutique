28e961233d59de57b02a8ca98840b3f96980f018
Damir Perisic <underscope@gmail.com>
2018-04-06 12:37:57 +0200

    Created basic ForgotPassword component

---
+++ client/common/components/auth/ForgotPassword.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div v-if="message" class="well">
+      <span>{{ message }}</span>
+    </div>
+    <div v-else>
+      <form @submit.prevent="submit">
+        <v-input v-model="email" name="email" validate="required|email"></v-input>
+        <button type="submit" class="button is-primary">Send reset email</button>
+        <div class="options">
+          <a @click="$router.go(-1)">Back</a>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { delay } from 'bluebird';
+import { mapActions } from 'vuex';
+import VInput from '@/common/components/form/VInput';
+
+export default {
+  data() {
+    return {
+      email: '',
+      message: null
+    };
+  },
+  methods: {
+    ...mapActions(['forgotPassword']),
+    submit() {
+      this.forgotPassword({ email: this.email })
+        .then(() => {
+          this.message = 'Reset email sent';
+          return delay(2000);
+        })
+        .then(() => this.$router.push('/'))
+        .catch(() => (this.message = 'Error'));
+    }
+  },
+  components: { VInput }
+};
+</script>
+
+
+<style lang="scss" scoped>
+.well {
+  font-size: 16px;
+}
+
+.button {
+  margin-top: 5px;
+}
+
+.options {
+  padding: 10px 0;
+
+  a {
+    display: inline-block;
+    font-size: 14px;
+  }
+}
+</style>
