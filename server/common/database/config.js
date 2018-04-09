6d2d5fb1436da15add2482ff2df734931182cc32
Dario Vladovic <d.vladimyr@gmail.com>
2018-04-09 20:54:37 +0200

    Print SQL query benchmark in development mode :wrench:

--- server/common/database/config.js
+++ server/common/database/config.js
@@ -2,9 +2,13 @@
 
 require('dotenv').config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
   url: process.env.DATABASE_URI,
   dialect: 'postgres',
   operatorsAliases: false,
-  migrationStorageTableName: 'sequelize_meta'
+  migrationStorageTableName: 'sequelize_meta',
+  // TODO: Use bunyan for SQL logging.
+  benchmark: !isProduction
 };
