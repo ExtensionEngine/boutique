9cc84f7dc9e98d73f9aee583dd29715f11eac55e
Damir Perisic <underscope@gmail.com>
2018-04-10 12:09:48 +0200

    Created Program model migration

---
+++ server/common/database/migrations/20180410092740-create-program.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('program', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    name: {
+      type: Sequelize.STRING,
+      field: 'name',
+      allowNull: false
+    },
+    description: {
+      type: Sequelize.STRING(2000),
+      field: 'description',
+      allowNull: false
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      field: 'created_at',
+      allowNull: false
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      field: 'updated_at',
+      allowNull: false
+    },
+    deletedAt: {
+      type: Sequelize.DATE,
+      field: 'deleted_at'
+    }
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('program')
+};
