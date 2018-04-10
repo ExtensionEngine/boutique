436d07b9cc2b0cf09cb628ad979a69909d150092
Marin Bareta <mbareta@extensionengine.com>
2018-04-10 13:16:19 +0200

    Add csv parse utility function

---
+++ server/common/util/csv.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const Promise = require('bluebird');
+const parse = require('csv-parse');
+
+const csvOptions = {
+  from: 1,
+  to: 10,
+  trim: true,
+  delimiter: ',',
+  columns: true
+};
+
+/**
+ * Accepts filePath string, reads file and returns error and parsed CSV data.
+ * @param {string} filePath
+ */
+module.exports = function parseCsv(filePath) {
+  if (!filePath) return Promise.reject(new Error('File path is required parameter.'));
+
+  return new Promise((resolve, reject) => {
+    const callback = (err, data) => (err ? reject(err) : resolve(data));
+    const parser = parse(csvOptions, callback);
+    fs.createReadStream(filePath).pipe(parser); // the CSV parser takes care of stream end and close
+  });
+};
