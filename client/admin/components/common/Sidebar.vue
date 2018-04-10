78e8e48e1b2273a408d2e1f7162946bb8a73c06a
Damir Perisic <underscope@gmail.com>
2018-04-10 13:35:12 +0200

    Configured program route

--- client/admin/components/common/Sidebar.vue
+++ client/admin/components/common/Sidebar.vue
@@ -1,13 +1,9 @@
 <template>
   <aside class="menu">
-    <p class="menu-label">Actions</p>
-    <ul class="menu-list">
-      <li><router-link to="/">Dashboard</router-link></li>
-    </ul>
     <p class="menu-label">Administration</p>
     <ul class="menu-list">
+      <li><router-link to="/">Programs</router-link></li>
       <li><router-link to="/users">Users</router-link></li>
-      <li><router-link to="/programs">Programs</router-link></li>
     </ul>
   </aside>
 </template>
