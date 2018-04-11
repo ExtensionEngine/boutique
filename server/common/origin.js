bc6150d69ba7e63949bceb45d10c46f0283138fd
Dario Vladovic <d.vladimyr@gmail.com>
2018-04-11 12:15:06 +0200

    Warn about HOSTNAME in production only :wrench:

--- server/common/origin.js
+++ server/common/origin.js
@@ -3,10 +3,12 @@
 const { hostname } = require('../config');
 const logger = require('./logger')();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = () => {
-  if (!hostname) {
-    logger.warn('⚠️  "HOSTNAME" is not set, using "Host" HTTP header.');
-  }
+  if (hostname) return middleware;
+  const message = '"HOSTNAME" is not set, using "Host" HTTP header.';
+  isProduction ? logger.warn('⚠️ ', message) : logger.info(message);
   return middleware;
 };
 
