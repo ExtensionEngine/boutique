3b8fbc4cadf1d153de43242a793e7f5ba4d1926c
Marin Bareta <mbareta@extensionengine.com>
2018-04-12 10:22:54 +0200

    Extract file upload form into a component

---
+++ client/admin/components/schools/Upload.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="school-upload">
+    <b v-if="isUploading"> (TODO: add icon) Uploading, please wait...</b>
+    <form v-else @submit.prevent="prepareUpload" method="POST" enctype="multipart/form-data">
+      <div class="file has-name is-fullwidth">
+        <label class="file-label">
+          <input ref="csv" @change="updateFileName" class="file-input" type="file" name="csv"/>
+          <span class="file-cta">
+            <span class="file-icon">
+              <i class="fas fa-upload"></i>
+            </span>
+            <span class="file-label">{{ label }}</span>
+          </span>
+          <span class="file-name">
+            {{ fileName || placeholder }}
+          </span>
+          <input type="submit" value="Upload" class="button is-primary" :disabled="!fileName"/>
+        </label>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  name: 'school-upload',
+  props: {
+    label: {
+      type: String,
+      default: 'NCES School Universe File'
+    },
+    placeholder: {
+      type: String,
+      default: 'Please select a file to upload'
+    }
+  },
+  data() {
+    return {
+      fileName: ''
+    };
+  },
+  computed: mapState('schools', ['isUploading']),
+  methods: {
+    ...mapActions('schools', ['upload']),
+    prepareUpload() {
+      const { files } = this.$refs.csv;
+      if (files.length === 0) return;
+      const data = new FormData(); // eslint-disable-line
+      data.append('csv', files[0], files[0].name);
+      this.upload({ path: 'import', data });
+    },
+    updateFileName({ target: { files } }) {
+      this.fileName = files[0].name;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.school-upload {
+  margin: 2rem auto;
+
+  input[type=submit] {
+    width: 8rem;
+    margin-left: 1rem;
+  }
+}
+</style>
--- client/admin/components/schools/index.vue
+++ client/admin/components/schools/index.vue
@@ -1,26 +1,7 @@
 <template>
   <div>
     <h1 class="title">Schools</h1>
-    <b v-if="isUploading"> (TODO: add icon) Uploading, please wait...</b>
-    <form v-else @submit.prevent="prepareUpload" method="POST" enctype="multipart/form-data">
-      <div class="file has-name is-fullwidth">
-        <label class="file-label">
-          <input ref="csv" @change="updateFileName" class="file-input" type="file" name="csv"/>
-          <span class="file-cta">
-            <span class="file-icon">
-              <i class="fas fa-upload"></i>
-            </span>
-            <span class="file-label">
-              NCES School Universe File
-            </span>
-          </span>
-          <span class="file-name">
-            {{ fileName || 'Please select a file to upload' }}
-          </span>
-          <input type="submit" value="Upload" class="button is-primary" disabled="!fileName"/>
-        </label>
-      </div>
-    </form>
+    <upload/>
     <table class="table is-fullwidth is-hoverable">
       <thead>
         <th>Name</th>
@@ -42,42 +23,17 @@
 
 <script>
 import { mapState, mapActions } from 'vuex';
+import Upload from './Upload';
 
 export default {
   name: 'school-list',
-  data() {
-    return {
-      fileName: ''
-    };
-  },
-  computed: mapState('schools',
-    {
-      isUploading: 'isUploading',
-      schools: 'items'
-    }
-  ),
-  methods: {
-    ...mapActions('schools', ['fetch', 'upload']),
-    prepareUpload() {
-      const { files } = this.$refs.csv;
-      if (files.length === 0) return;
-      const data = new FormData(); // eslint-disable-line
-      data.append('csv', files[0], files[0].name);
-      this.upload({ path: 'import', data });
-    },
-    updateFileName({ target: { files } }) {
-      this.fileName = files[0].name;
-    }
-  },
+  computed: mapState('schools', { schools: 'items' }),
+  methods: mapActions('schools', ['fetch']),
   mounted() {
     this.fetch();
+  },
+  components: {
+    Upload
   }
 };
 </script>
-
-<style lang="scss" scoped>
-input[type=submit] {
-  width: 8rem;
-  margin-left: 1rem;
-}
-</style>
