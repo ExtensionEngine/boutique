03c231db1002c240bcf97341e9bfafeafda815c0
Marin Bareta <mbareta@extensionengine.com>
2018-04-12 12:11:14 +0200

    Extract district getters to separate file

---
+++ client/admin/store/modules/districts/getters.js
@@ -0,0 +1,1 @@
+export const array = state => Object.values(state.items);
--- client/admin/store/modules/districts/index.js
+++ client/admin/store/modules/districts/index.js
@@ -1,4 +1,5 @@
 import * as actions from './actions';
+import * as getters from './getters';
 import * as mutations from './mutations';
 
 const state = {
@@ -10,9 +11,6 @@ export default {
   namespaced: true,
   state,
   actions,
-  mutations,
-  getters: {
-    // TODO: extract into file
-    array: state => Object.values(state.items)
-  }
+  getters,
+  mutations
 };
