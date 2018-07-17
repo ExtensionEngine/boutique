7fba4c7e3167b3739ac43934cf500f6a3819dc9b
Dario Vladovic <d.vladimyr@gmail.com>
2018-07-17 14:54:41 +0200

    Add eslint-vue-plugin :wrench:

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,14 @@ const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
   root: true,
-  parserOptions: { sourceType: 'module' },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2017
+  },
   // https://github.com/Flet/eslint-config-semistandard
-  extends: 'semistandard',
+  extends: ['semistandard', 'plugin:vue/recommended'],
   // required to lint *.vue files
-  plugins: ['html'],
+  plugins: ['vue'],
   rules: {
     indent: ['error', 2, {
       SwitchCase: 1,
@@ -20,6 +23,46 @@ module.exports = {
     'space-before-function-paren': ['error', {
       anonymous: 'always',
       named: 'never'
+    }],
+    // Vue rules
+    'vue/html-self-closing': 'off',
+    'vue/attribute-hyphenation': 'off',
+    'vue/max-attributes-per-line': ['error', { singleline: 5 }],
+    'vue/name-property-casing': ['error', 'kebab-case'],
+    // TODO: Add order for custom directives once supported
+    'vue/attributes-order': ['error', {
+      order: [
+        'DEFINITION',
+        'LIST_RENDERING',
+        'CONDITIONALS',
+        'RENDER_MODIFIERS',
+        'UNIQUE',
+        'BINDING',
+        'EVENTS',
+        'CONTENT',
+        'GLOBAL',
+        'OTHER_ATTR'
+      ]
+    }],
+    'vue/order-in-components': ['error', {
+      order: [
+        'el',
+        'name',
+        ['template', 'render', 'renderError'],
+        ['parent', 'functional', 'delimiters', 'comments'],
+        'extends',
+        'mixins',
+        'inheritAttrs',
+        'model',
+        ['props', 'propsData'],
+        'data',
+        'computed',
+        'methods',
+        'watch',
+        'LIFECYCLE_HOOKS',
+        ['directives', 'filters'],
+        'components'
+      ]
     }]
   }
 };
