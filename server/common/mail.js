3c51c09b2f8778fbd43fe35796516eb79841d841
Tereza Karabatic <tkarabatic2@gmail.com>
2018-08-13 18:14:29 +0200

    Add HTML template to welcome and password reset e-mails

--- server/common/mail.js
+++ server/common/mail.js
@@ -5,10 +5,17 @@ const { parse: parseUrl } = require('url');
 const { promisify } = require('util');
 const { role } = require('../../common/config');
 const email = require('emailjs');
+const fs = require('fs');
+const path = require('path');
+const pupa = require('pupa');
 
 const server = email.server.connect(config);
 const send = promisify(server.send.bind(server));
 const isAdmin = user => user.role && user.role === role.ADMIN;
+const baseEmailTemplate = fs.readFileSync(
+  path.join(__dirname, './mail-templates/base.html'),
+  'utf-8'
+);
 
 module.exports = {
   send,
@@ -19,29 +26,49 @@ module.exports = {
 function invite(user, { origin }) {
   const href = resetUrl(origin, user);
   const { hostname } = parseUrl(href);
-  const message = `
-    An account has been created for you on ${hostname}.
-    Please click <a href="${href}">here</a> to complete your registration.`;
+  const message = pupa(baseEmailTemplate, {
+    title: 'LMS - New User Invitation',
+    headerText: 'Welcome!',
+    userName: user.firstName,
+    bodyText: `
+      An account has been created for you on <span style="font-style: italic; font-weight: bold;">${hostname}</span>.
+      We're excited to have you here! Before you can get started, you need to complete your registration.
+      Please do so by clicking the button below:
+    `,
+    button: {
+      url: href,
+      text: 'Confirm Account'
+    }
+  });
 
   return send({
     from: `LMS <${config.sender}>`,
     to: user.email,
     subject: 'Invite',
-    attachment: [{ data: `<html>${message}</html>`, alternative: true }]
+    attachment: [{ data: message, alternative: true }]
   });
 }
 
 function resetPassword(user, { origin }) {
   const href = resetUrl(origin, user);
-  const message = `
-    You requested password reset.
-    Please click <a href="${href}">here</a> to complete the reset process.`;
+  const message = pupa(baseEmailTemplate, {
+    title: 'LMS - Password Reset',
+    headerText: 'Password Reset Required',
+    userName: user.firstName,
+    bodyText: `
+      You have requested a password reset. Please click on the button below to complete the process:
+    `,
+    button: {
+      url: href,
+      text: 'Reset Password'
+    }
+  });
 
   return send({
     from: `LMS <${config.sender}>`,
     to: user.email,
     subject: 'Reset password',
-    attachment: [{ data: `<html>${message}</html>`, alternative: true }]
+    attachment: [{ data: message, alternative: true }]
   });
 }
 
