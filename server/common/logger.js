1c5372497ba7caee3e9204855ce836abd38496b3
Dario Vladovic <d.vladimyr@gmail.com>
2018-08-30 17:13:33 +0200

    Address audit warnings :tada:

--- server/common/logger.js
+++ server/common/logger.js
@@ -1,10 +1,27 @@
 'use strict';
 
-const bunyan = require('bunyan');
+const Logger = withEmoji(require('bunyan'));
 const pkg = require('../../package.json');
+const stripEmoji = require('emoji-strip');
+
+const isMacOS = process.platform === 'darwin';
+const isString = arg => typeof arg === 'string';
+
 const loggers = {};
 
 module.exports = (name = pkg.name) => {
-  if (loggers[name]) return loggers.name;
-  return bunyan.createLogger({ name });
+  if (!loggers[name]) loggers[name] = new Logger({ name });
+  return loggers[name];
 };
+
+function withEmoji(Logger) {
+  Object.keys(Logger.levelFromName).forEach(name => {
+    const log = Logger.prototype[name];
+    Logger.prototype[name] = function (...args) {
+      if (isMacOS) return log.apply(this, args);
+      args = args.map(it => !isString(it) ? it : stripEmoji(it));
+      return log.apply(this, args);
+    };
+  });
+  return Logger;
+}
