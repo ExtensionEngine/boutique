652d7e22ec943eabb861e88bc201a2957a4a9a8d
franosincic <fsincic@extensionengine.com>
2018-09-11 15:05:54 +0200

    Moved all content routes in content delivery router

--- server/cohort/index.js
+++ server/cohort/index.js
@@ -1,16 +1,14 @@
 'use strict';
 
 const { authorize } = require('../common/auth/mw');
-const { Cohort, ContentRepo, Enrollment } = require('../common/database');
+const { Cohort } = require('../common/database');
+const contentRouter = require('../content-repo/content-delivery.router');
 const { createError } = require('../common/errors');
 const ctrl = require('./cohort.controller');
-const contentCtrl = require('../content-repo/content-delivery.controller');
-const forEach = require('lodash/forEach');
 const HttpStatus = require('http-status');
-const includes = require('lodash/includes');
 const router = require('express').Router();
 
-const { NOT_FOUND, UNAUTHORIZED } = HttpStatus;
+const { NOT_FOUND } = HttpStatus;
 
 router
   .use('/:id*', getCohort)
@@ -18,47 +16,13 @@ router
   .post('/', authorize(), ctrl.create)
   .get('/:id', authorize(), ctrl.get)
   .patch('/:id', authorize(), ctrl.patch)
-  .use('/:id/content*', hasAccess)
-  .get('/:id/content', contentCtrl.list)
-  .use('/:id/content/:contentId*', getRepo)
-  .get('/:id/content/:contentId', contentCtrl.get);
+  .use('/:id/content', contentRouter.router);
 
 function getCohort(req, res, next) {
   return Cohort.findById(req.params.id, { paranoid: false })
     .then(cohort => cohort || createError(NOT_FOUND, 'Not found!'))
     .then(cohort => {
       req.cohort = cohort;
-      next();
-    });
-}
-
-function hasAccess(req, res, next) {
-  const { user, cohort } = req;
-  if (user.isAdmin()) return next();
-  const opts = { where: { studentId: user.id }, attributes: ['cohortId'] };
-  return Enrollment.all(opts)
-    .then(enrollments => {
-      let enrolledToCohort = false;
-      forEach(enrollments, it => {
-        if (it.cohortId === cohort.id) {
-          enrolledToCohort = true;
-          return false;
-        }
-      });
-      if (enrolledToCohort) {
-        next();
-      } else {
-        return createError(UNAUTHORIZED, 'Access restricted');
-      }
-    });
-}
-
-function getRepo(req, res, next) {
-  const opts = { where: { id: req.params.contentId, cohortId: req.cohort.id } };
-  return ContentRepo.findOne(opts)
-    .then(repo => repo || createError(NOT_FOUND, 'Not found!'))
-    .then(repo => {
-      req.repo = repo;
       next();
     });
 }
---
+++ server/content-repo/content-delivery.router.js
@@ -0,0 +1,49 @@
+const { ContentRepo, Enrollment } = require('../common/database');
+const { createError } = require('../common/errors');
+const ctrl = require('./content-delivery.controller');
+const forEach = require('lodash/forEach');
+const HttpStatus = require('http-status');
+const router = require('express').Router({ mergeParams: true });
+
+const { NOT_FOUND, UNAUTHORIZED } = HttpStatus;
+
+router
+  .use('/*', hasAccess)
+  .get('/', ctrl.list)
+  .use('/:contentId*', getRepo)
+  .get('/:contentId', ctrl.get);
+
+function hasAccess(req, res, next) {
+  const { user, cohort } = req;
+  if (user.isAdmin()) return next();
+  const opts = { where: { studentId: user.id }, attributes: ['cohortId'] };
+  return Enrollment.all(opts)
+    .then(enrollments => {
+      let enrolledToCohort = false;
+      forEach(enrollments, it => {
+        if (it.cohortId === cohort.id) {
+          enrolledToCohort = true;
+          return false;
+        }
+      });
+      if (enrolledToCohort) {
+        next();
+      } else {
+        return createError(UNAUTHORIZED, 'Access restricted');
+      }
+    });
+}
+
+function getRepo(req, res, next) {
+  const opts = { where: { id: req.params.contentId, cohortId: req.params.id } };
+  return ContentRepo.findOne(opts)
+    .then(repo => repo || createError(NOT_FOUND, 'Not found!'))
+    .then(repo => {
+      req.repo = repo;
+      next();
+    });
+}
+
+module.exports = {
+  router
+};
