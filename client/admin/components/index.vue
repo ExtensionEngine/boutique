033bec4fc924d2eae8f5e804554a886ad072cb0a
Damir Perisic <underscope@gmail.com>
2018-09-27 13:39:33 +0200

    Vuetify root container

--- client/admin/components/index.vue
+++ client/admin/components/index.vue
@@ -1,40 +1,29 @@
 <template>
-  <div class="app-container">
-    <header class="app-header">
-      <navbar/>
-    </header>
-    <main class="app-content container">
-      <div class="columns">
-        <div class="column is-2">
-          <sidebar/>
-        </div>
-        <div class="column is-10">
-          <router-view></router-view>
-        </div>
-      </div>
-    </main>
-  </div>
+  <v-app>
+    <sidebar :drawer="drawer"/>
+    <navbar :drawer.sync="drawer"/>
+    <v-content>
+      <v-container fluid fill-height class="grey lighten-4">
+        <router-view/>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 import Navbar from '@/admin/components/common/Navbar';
 import Sidebar from '@/admin/components/common/Sidebar';
 
 export default {
   name: 'admin',
+  data() {
+    return { drawer: true };
+  },
+  methods: mapActions('cohorts', ['fetch']),
+  created() {
+    this.fetch();
+  },
   components: { Navbar, Sidebar }
 };
 </script>
-
-<style lang="scss" scoped>
-.app-container {
-  display: flex;
-  height: 100%;
-  padding: 32px 0;
-  flex-direction: column;
-}
-
-.app-content {
-  flex: 1;
-}
-</style>
