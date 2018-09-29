986933ff72522cc87745e640d284f469269993d4
Damir Perisic <underscope@gmail.com>
2018-09-29 22:17:01 +0200

    Created reusable ConfirmationDialog :goat:

---
+++ client/admin/components/common/ConfirmationDialog.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-dialog v-model="show" max-width="500">
+    <v-card>
+      <v-card-title class="headline">{{ heading }}</v-card-title>
+      <v-card-text>{{ message }}</v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn @click.native="close" flat>Cancel</v-btn>
+        <v-btn @click.native="action" color="red" flat>Yes</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'confirmation-dialog',
+  props: {
+    visible: { type: Boolean, default: false },
+    heading: { type: String, default: '' },
+    message: { type: String, default: 'Are you sure?' },
+    action: { type: Function, required: true }
+  },
+  computed: {
+    show: {
+      get() {
+        return this.visible;
+      },
+      set(value) {
+        if (!value) this.close();
+      }
+    }
+  },
+  methods: {
+    close() {
+      this.$emit('update:visible', false);
+    }
+  }
+};
+</script>
