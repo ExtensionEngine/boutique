10d2e999b4aa812c74c40d781fcdbadaadef99d4
Damir Perisic <underscope@gmail.com>
2018-10-06 17:25:44 +0200

    Tweaked Navbar style :lipstick:
    - Added logo, user & logout icons
    - Added shadow and tweaked fonts

--- client/student/components/common/Navbar.vue
+++ client/student/components/common/Navbar.vue
@@ -5,7 +5,7 @@
     aria-label="main navigation">
     <div class="navbar-brand">
       <router-link to="/" class="navbar-item">
-        Boutique
+        <span class="mdi mdi-shopping"></span>Boutique
       </router-link>
     </div>
     <div v-if="user" class="navbar-menu">
@@ -16,13 +16,13 @@
           class="navbar-item">
           Dashboard
         </router-link>
-        <div class="navbar-item has-dropdown is-hoverable">
-          <a class="navbar-link">
-            {{ user.email }}
+        <div class="navbar-item has-dropdown is-hoverable user-dropdown">
+          <a href="#" class="navbar-link">
+            <span class="mdi mdi-account-circle"></span>{{ user.email }}
           </a>
           <div class="navbar-dropdown is-right">
-            <a class="navbar-item">
-              <a @click="logout" href="#">Logout</a>
+            <a @click.prevent="logout" href="#" class="navbar-item">
+              <span class="mdi mdi-logout"></span>Logout
             </a>
           </div>
         </div>
@@ -48,3 +48,48 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.navbar {
+  min-height: 4rem;
+  box-shadow:
+    0 2px 4px -1px rgba(0,0,0,0.2),
+    0 4px 5px 0 rgba(0,0,0,0.14),
+    0 1px 10px 0 rgba(0,0,0,0.12);
+}
+
+.navbar-item {
+  font-size: 1.25rem;
+  font-weight: 300;
+}
+
+.user-dropdown {
+  margin-left: 2rem;
+
+  .navbar-dropdown {
+    padding: 0;
+  }
+
+  .navbar-item {
+    padding: 0.5rem 1rem;
+    font-size: 1rem;
+  }
+
+  .mdi {
+    padding-right: 0.5rem;
+    font-size: 1.5rem;
+  }
+}
+
+.navbar-brand .navbar-item {
+  padding: 0 1.5rem;
+  font-size: 1.5rem;
+  font-weight: 300;
+  line-height: 2.5rem;
+
+  .mdi {
+    margin-right: 0.5rem;
+    font-size: 2rem;
+  }
+}
+</style>
