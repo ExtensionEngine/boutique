7a0e44cc0e26df3c44e570387dd93e81b8d8f27a
borna223 <bburaz00@fesb.hr>
2018-10-11 08:18:53 +0200

    Add 'Delete program' confirmation dialog

--- client/admin/components/Program/Settings/index.vue
+++ client/admin/components/Program/Settings/index.vue
@@ -1,17 +1,47 @@
 <template>
   <div class="mt-3">
-    Settings
+    <v-toolbar color="#f5f5f5" flat>
+      <v-spacer/>
+      <v-btn v-if="program" @click="remove(program)" color="error" outline>Delete Program</v-btn>
+      <confirmation-dialog
+        :visible.sync="confirmationDialog"
+        :action="confirmationAction"
+        heading="Delete program"
+        message="Are you sure you want to delete program?"/>
+    </v-toolbar>
   </div>
+
 </template>
 
 <script>
+import ConfirmationDialog from '@/admin/components/common/ConfirmationDialog';
+import { mapActions } from 'vuex';
+
 export default {
-  name: 'settings'
+  name: 'settings',
+  props: { programId: { type: Number, required: true } },
+  data() {
+    return {
+      confirmationDialog: null,
+      confirmationAction: null
+    };
+  },
+  computed: {
+    program() {
+      return this.$parent.program;
+    }
+  },
+  methods: {
+    ...mapActions('programs', { removeProgram: 'remove' }),
+    remove(program) {
+      this.confirmationDialog = true;
+      this.confirmationAction = () => {
+        // todo: add 'delete route' and redirect
+        // this.removeProgram(program);
+      };
+    }
+  },
+  components: { ConfirmationDialog }
+
 };
 </script>
-
-<style lang="scss" scoped>
-.actions {
-  width: 250px;
-}
-</style>
