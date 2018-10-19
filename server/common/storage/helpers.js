58f57c4c129df4188810b960b0e605e76164436d
franosincic <fsincic@extensionengine.com>
2018-10-19 10:55:56 +0200

    Fix and cleanup

--- server/common/storage/helpers.js
+++ server/common/storage/helpers.js
@@ -6,23 +6,19 @@ const Promise = require('bluebird');
 const values = require('lodash/values');
 
 const Storage = createStorage(config.storage);
-const isComposite = it => it.data.embeds;
 
 function resolveAsset(asset) {
-  if (!asset.data || !asset.data.url) return Promise.resolve(asset);
-
   function getUrl(key) {
     return Storage.getFileUrl(key)
       .then(url => (asset.data.url = url))
       .then(() => asset);
   }
-
   return Storage.fileExists(asset.data.url)
     .then(exists => exists ? getUrl(asset.data.url) : asset);
 }
 
 function resolvePrimitive(primitive) {
-  if (!primitive.type === 'IMAGE') return Promise.resolve(primitive);
+  if (primitive.type !== 'IMAGE') return Promise.resolve(primitive);
   return resolveAsset(primitive);
 }
 
@@ -32,14 +28,12 @@ function resolveComposite(composite) {
 }
 
 function resolveElement(element) {
-  return isComposite(element)
+  return element.data.embeds
     ? resolveComposite(element)
     : resolvePrimitive(element);
 }
 
 function resolveContainer(container) {
-  if (!container.elements) return Promise.resolve(container);
-
   return Promise.each(container.elements, resolveElement)
     .then(() => container);
 }
