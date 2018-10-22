f87ad76ddffe70a70ac99b5ff4d03061ce54ff56
Dario Vladovic <d.vladimyr@gmail.com>
2018-10-22 19:43:17 +0200

    Remove alterEnum sequelize helper

--- server/common/database/helpers.js
+++ server/common/database/helpers.js
@@ -4,19 +4,9 @@ const inRange = require('lodash/inRange');
 const notEmpty = input => input.length > 0;
 
 module.exports = {
-  alterEnum,
   getValidator,
   setLogging
 };
-
-// NOTE: Enables safe altering of ENUM values: https://git.io/fxzeS
-function alterEnum(queryInterface, Sequelize, table, column, options = {}) {
-  const { QueryGenerator, sequelize } = queryInterface;
-  const str = { type: Sequelize.STRING, allowNull: false };
-  return queryInterface.changeColumn(table, column, str)
-    .then(() => sequelize.query(QueryGenerator.pgEnumDrop(table, column)))
-    .then(() => queryInterface.changeColumn(table, column, options));
-}
 
 function getValidator(Model, attribute) {
   return function validate(input) {
