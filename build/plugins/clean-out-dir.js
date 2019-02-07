76d7ee6f293ada069abe548e49d9ffaf9c9a75cb
Dario Vladovic <d.vladimyr@gmail.com>
2019-02-07 22:29:01 +0100

    Don't use `clean-plugin` in development mode :bug:

--- build/plugins/clean-out-dir.js
+++ build/plugins/clean-out-dir.js
@@ -4,9 +4,10 @@ exports.name = 'clean-out-dir';
 
 exports.apply = (api, opts = {}) => {
   api.hook('createWebpackChain', config => {
-    if (!api.config.output.clean) return;
+    if (!api.isProd || !api.config.output.clean) return;
     const path = api.resolveOutDir();
     opts.root = opts.root || api.cwd;
+    opts.verbose = Boolean(api.args.options.debug);
     config
       .plugin('clean-out-dir')
       .use(require.resolve('clean-webpack-plugin'), [path, opts]);
