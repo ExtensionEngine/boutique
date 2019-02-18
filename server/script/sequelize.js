b8f51ca9f1efa07cebe1fa35f9111a6347377852
Dario Vladovic <d.vladimyr@gmail.com>
2019-02-18 22:33:48 +0100

    Search for config inside cwd :wrench:

--- server/script/sequelize.js
+++ server/script/sequelize.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const config = require('../../sequelize.config.js');
 const dargs = require('dargs');
 const minimist = require('minimist');
+const path = require('path');
+const safeRequire = require('safe-require');
 
 const actions = [
   'migrate',
@@ -12,20 +13,33 @@ const actions = [
 ];
 const isAction = cmd => actions.some(it => cmd.startsWith(it));
 
+// Load config.
+const config = safeRequire(path.join(process.cwd(), 'sequelize.config.js'));
+if (!config) {
+  console.error('Error: `sequelize.config.js` not found');
+  process.exit(1);
+}
+
 const argv = minimist(process.argv.slice(2));
 process.argv.length = 2;
 
-// Resolve commands/args.
-let [head = '', ...rest] = argv._;
-if (isAction(head)) head = `db:${head}`;
-process.argv.push(head, ...rest);
+// Resolve command with arguments.
+const args = getArgs(argv);
+process.argv.push(...args);
 
-// Resolve flags.
+// Resolve options.
 const options = Object.assign({}, config, getOptions(argv));
 process.argv.push(...dargs(options));
 
 // Make it rain!
 require('sequelize-cli/lib/sequelize');
+
+function getArgs(argv) {
+  let [cmd, ...args] = argv._;
+  if (!cmd) return args;
+  if (isAction(cmd)) cmd = `db:${cmd}`;
+  return [cmd, ...args];
+}
 
 function getOptions(argv) {
   return reduce(argv, (acc, val, key) => {
