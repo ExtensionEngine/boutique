07f2a2f8dbe3a7fa1c0a65d293ad93ec27ba737b
rkusan00 <renato.kusanovic@gmail.com>
2019-03-06 14:19:46 +0100

    Modified route to save json and send id

--- server/preview/index.js
+++ server/preview/index.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const router = require('express').Router();
+const fs = require('fs');
 
 router
-  .get('/preview/:programId/:repositoryId/:containerId', getPreview);
+  .post('/preview', getPreview);
 
 function getPreview(req, res) {
-  const { programId, repositoryId, containerId } = req.params;
-  return res.redirect(`/#/previewComponent/${programId}/${repositoryId}/${containerId}`);
+  const path = `${__dirname}/../../common/${req.body.id}.json`;
+  const data = JSON.stringify(req.body);
+  fs.writeFileSync(path, data);
+  return res.redirect(`/#/previewComponent/${req.body.id}`);
 }
 
 module.exports = { router };
