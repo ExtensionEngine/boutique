2a3db7149120296b45976850bf25804a9f28a64a
rkusan00 <renato.kusanovic@gmail.com>
2019-03-06 18:32:21 +0100

    Unlink files after sending

--- server/preview/index.js
+++ server/preview/index.js
@@ -1,16 +1,24 @@
 'use strict';
 
 const router = require('express').Router();
-const fs = require('fs');
+const fs = require('fs').promises;
 
 router
-  .post('/preview', getPreview);
+  .post('/preview', postPreview)
+  .get('/preview/:containerId', getPreview);
 
-function getPreview(req, res) {
-  const path = `${__dirname}/../../common/${req.body.id}.json`;
-  const data = JSON.stringify(req.body);
-  fs.writeFileSync(path, data);
+async function postPreview(req, res) {
+  const path = `${__dirname}/${req.body.id}.json`;
+  await fs.writeFile(path, JSON.stringify(req.body));
   return res.redirect(`/#/previewComponent/${req.body.id}`);
+}
+
+async function getPreview(req, res) {
+  const { containerId } = req.params;
+  const path = `${__dirname}/${containerId}.json`;
+  const data = await fs.readFile(path);
+  fs.unlink(path);
+  return res.jsend.success(JSON.parse(data));
 }
 
 module.exports = { router };
