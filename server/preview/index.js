44c803ea4273e5f6f2ecd881e35ff751df9de74d
rkusan00 <renato.kusanovic@gmail.com>
2019-03-08 12:50:47 +0100

    Replaced fspromise with util promises

--- server/preview/index.js
+++ server/preview/index.js
@@ -1,7 +1,11 @@
 'use strict';
 
 const router = require('express').Router();
-const fs = require('fs').promises;
+const fs = require('fs');
+const { promisify } = require('util');
+
+const writeFile = promisify(fs.writeFile);
+const readFile = promisify(fs.readFile);
 
 router
   .post('/preview', postPreview)
@@ -9,15 +13,17 @@ router
 
 async function postPreview(req, res) {
   const path = `${__dirname}/${req.body.id}.json`;
-  await fs.writeFile(path, JSON.stringify(req.body));
+  await writeFile(path, JSON.stringify(req.body));
   return res.redirect(`/#/previewComponent/${req.body.id}`);
 }
 
 async function getPreview(req, res) {
   const { containerId } = req.params;
   const path = `${__dirname}/${containerId}.json`;
-  const data = await fs.readFile(path);
-  fs.unlink(path);
+  const data = await readFile(path, 'utf8');
+  fs.unlink(path, (err) => {
+    if (err) throw err;
+  });
   return res.jsend.success(JSON.parse(data));
 }
 
