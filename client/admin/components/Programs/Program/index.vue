b018372ad1057c479639f66a3321129374655e11
underscope <underscope@gmail.com>
2021-01-28 18:16:10 +0100

    Program root cleanup :lipstick:

--- client/admin/components/Programs/Program/index.vue
+++ client/admin/components/Programs/Program/index.vue
@@ -1,17 +1,5 @@
 <template>
-  <div :key="programId" class="ma-5">
-    <v-breadcrumbs v-if="program" :items="breadcrumbs" class="pb-1" />
-    <v-tabs background-color="transparent" class="ml-2">
-      <v-tab
-        v-for="({ name, label }) in tabs"
-        :key="name"
-        :to="{ name, params: { programId } }"
-        exact ripple>
-        {{ label }}
-      </v-tab>
-    </v-tabs>
-    <router-view v-if="program" :program="program" />
-  </div>
+  <router-view v-if="program" :key="programId" :program="program" />
 </template>
 
 <script>
@@ -19,22 +7,13 @@ import { mapActions, mapState } from 'vuex';
 import find from 'lodash/find';
 
 export default {
-  name: 'program',
+  name: 'program-container',
   props: {
     programId: { type: Number, required: true }
   },
   computed: {
     ...mapState('programs', { programs: 'items' }),
-    program: vm => find(vm.programs, { id: vm.programId }),
-    breadcrumbs: ({ program }) => [
-      { text: 'Programs', disabled: true },
-      { text: program.name, disabled: true }
-    ],
-    tabs: () => [
-      { name: 'enrollments', label: 'Enrollments' },
-      { name: 'importedContent', label: 'Content' },
-      { name: 'programSettings', label: 'Settings' }
-    ]
+    program: vm => find(vm.programs, { id: vm.programId })
   },
   methods: mapActions('programs', ['get']),
   created() {
@@ -42,13 +21,3 @@ export default {
   }
 };
 </script>
-
-<style lang="scss" scoped>
-.v-tab {
-  margin-left: 0 !important;
-}
-
-.v-breadcrumbs {
-  padding-left: 1.125rem;
-}
-</style>
