1e37934363002169dd5ad4a45001db130ed2ac46
Luka Saric <rpg.ari7@gmail.com>
2021-03-04 16:38:42 +0100

    Required async errs before main router mount

--- server/app.js
+++ server/app.js
@@ -14,13 +14,15 @@ const jsend = require('jsend').middleware;
 const morgan = require('morgan');
 const nocache = require('nocache');
 const origin = require('./common/origin');
-const router = require('./router');
 const logger = require('./common/logger')();
 
 // eslint-disable-next-line require-sort/require-sort
 require('express-async-errors');
 
+const router = require('./router');
+
 const app = express();
+
 app.use(helmet());
 app.use(cors({
   origin: config.cors.allowedOrigins,
