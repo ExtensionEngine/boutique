aa5617ce27a855f426f900f655fc1712b997a644
Luka Saric <rpg.ari7@gmail.com>
2021-04-14 12:24:23 +0200

    User group membership hooks cleanup :nail_care:

--- server/user-group-member/hooks.js
+++ server/user-group-member/hooks.js
@@ -4,10 +4,10 @@ exports.add = (UserGroupMember, Hooks, db) => {
   const { Enrollment, OfferingUserGroup } = db;
 
   UserGroupMember.addHook(Hooks.afterCreate, async member => {
-    const { userId: learnerId, userGroupId } = member;
+    const { userId, userGroupId } = member;
     const where = { userGroupId };
     const enrollments = await OfferingUserGroup.findAll({ where })
-      .map(({ offeringId }) => ({ learnerId, offeringId }));
+      .map(({ offeringId }) => ({ learnerId: userId, offeringId }));
     return Enrollment.bulkCreate(enrollments);
   });
 
@@ -16,26 +16,28 @@ exports.add = (UserGroupMember, Hooks, db) => {
     if (isRestored) return restoreEnrollments(member, db);
     const isUserChanged = member.changed('userId');
     if (!isUserChanged) return;
-    const { userId: oldLearnerId } = member._previousDataValues;
-    const { userId: learnerId, userGroupId } = member;
-    const offeringIds = await OfferingUserGroup.findAll({ where: { userGroupId } })
-      .map(({ offeringId }) => offeringId);
-    const where = { learnerId: oldLearnerId, enrollmentOfferingId: offeringIds };
-    return Enrollment.update({ learnerId }, { where });
+    const { userId, userGroupId, _previousDataValues } = member;
+    const { userId: oldLUserId } = _previousDataValues;
+    const offeringIds = await getOfferingIds(userGroupId);
+    const where = { learnerId: oldLUserId, enrollmentOfferingId: offeringIds };
+    return Enrollment.update({ learnerId: userId }, { where });
   });
 
   UserGroupMember.addHook(Hooks.afterDestroy, async member => {
-    const { userId: learnerId, userGroupId } = member;
-    const offeringIds = await OfferingUserGroup.findAll({ where: { userGroupId } })
-      .map(({ offeringId }) => offeringId);
-    const where = { learnerId, enrollmentOfferingId: offeringIds };
+    const { userId, userGroupId } = member;
+    const offeringIds = await getOfferingIds(userGroupId);
+    const where = { learnerId: userId, enrollmentOfferingId: offeringIds };
     return Enrollment.destroy({ where });
   });
 
   async function restoreEnrollments({ userId, userGroupId }) {
-    const offeringIds = await OfferingUserGroup.findAll({ where: { userGroupId } })
-      .map(({ offeringId }) => offeringId);
+    const offeringIds = await getOfferingIds(userGroupId);
     const where = { learnerId: userId, enrollmentOfferingId: offeringIds };
     return Enrollment.update({ deletedAt: null }, { where, paranoid: false });
+  }
+
+  function getOfferingIds(userGroupId) {
+    const opts = { where: { userGroupId } };
+    return OfferingUserGroup.findAll(opts).map(({ offeringId }) => offeringId);
   }
 };
